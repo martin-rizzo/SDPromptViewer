1b6bb012186ef68849875f3a191e392aec7ad1ad
Martin Rizzo <martinrizzo@gmail.com>
2023-03-30 21:59:42 -0300

    feat: Implement a parser for Stable Diffusion WebUI generation parameters
    This commit introduces a parser that takes in text as input and
    outputs the generation parameters used by Stable Diffusion WebUI.
    
    It's worth noting that the implementation of the parser in pure C
    has resulted in a slightly complex design. In the future, it may
    be beneficial to re-implement the parser using a more modern and
    higher-level language that offers better safety guarantees and
    abstractions.

--- utils_parser.h
+++ utils_parser.h
@@ -1,6 +1,14 @@
 
 #include <string.h>
 
+#define IS_ALPHA_SPACE(x) parse_sd_params_is_alpha_space(x)
+
+static int
+parse_sd_params_is_alpha_space(int ch) {
+    return ('a'<=ch && ch<='z') || ('A'<=ch && ch<='Z') ||
+           ('0'<=ch && ch<='9') || (ch==' ');
+}
+    
 typedef void (*ParseParametersCallback)(const char *key,
                                         int         key_size,
                                         const char *value,
@@ -22,27 +30,78 @@ parsed_param_trim(const char **inout_text, int *inout_text_size) {
 }
 
 static void
-parsed_param_key(const char **inout_text, int *inout_text_size) {
+parsed_param_key(const char **out_key,
+                 int         *out_key_size,
+                 const char  *param,
+                 int          param_size)
+{
     const char *ptr; int size, size_limit;
-    ptr = (*inout_text); size = 0; size_limit = (*inout_text_size);
+    ptr = param; size = 0; size_limit = param_size;
     while( size<size_limit && ptr[size]!=KEYVALUE_SEPARATOR ) { ++size; }
     parsed_param_trim( &ptr, &size );
-    (*inout_text) = ptr; (*inout_text_size) = size;
+    (*out_key) = ptr; (*out_key_size) = size;
 }
 
 static void
-parsed_param_value(const char **inout_text, int *inout_text_size) {
+parsed_param_value(const char **out_value,
+                   int         *out_value_size,
+                   const char  *param,
+                   int          param_size)
+{
     const char *ptr; int size;
-    ptr = (*inout_text); size = (*inout_text_size);
+    ptr = param; size = param_size;
     while( size>0 && ptr[0]!=KEYVALUE_SEPARATOR ) { --size; ++ptr; }
     if   ( size>0 && ptr[0]==KEYVALUE_SEPARATOR ) { --size; ++ptr; }
     parsed_param_trim( &ptr, &size );
-    (*inout_text) = ptr; (*inout_text_size) = size;
+    (*out_value) = ptr; (*out_value_size) = size;
 }
 
-static char*
+static const int
+parse_next_param(const char **out_param,
+                 int         *out_param_size,
+                 const char **inout_buffer,
+                 int         *inout_buffer_size)
+{
+    const char *ptr, *param; int size, param_size;
+    ptr  = (*inout_buffer);
+    size = (*inout_buffer_size);
+
+    /* (alfanum + spaces) ':' (spaces)     (alfanum + spaces)      ',' */
+    /* (alfanum + spaces) ':' (spaces) '"' (anything) '"' (spaces) ',' */
+    
+    param = ptr;
+    while( size>0 && IS_ALPHA_SPACE(*ptr) ) { --size; ++ptr; }
+    if( size==0 || *ptr!=':' ) { return 0; }
+    --size; ++ptr;
+    while( size>0 && *ptr!=',' ) { --size; ++ptr; }
+    param_size = (ptr - param);
+    
+    if( size>0 ) { --size; ++ptr; }
+    (*out_param        ) = param;
+    (*out_param_size   ) = param_size;
+    (*inout_buffer     ) = ptr;
+    (*inout_buffer_size) = size;
+    return 1;
+}
+
+static const char*
 parse_params_find_last_line(const char *text, int text_size) {
-    return NULL;
+    const char *ptr, *param, *lastline; int size, param_size;
+    
+    if( text_size==0 ) { return NULL; }
+    if( text_size==1 && *text=='\n' ) { return NULL; }
+    
+    /* find the beginning of the last line */
+    ptr = &text[text_size-2]; size = 2;
+    while( size<text_size && *ptr!='\n' ) { ++size; --ptr; }
+    if( *ptr!='\n' ) { return NULL; }
+    --size; ++ptr;
+
+    /* verify the last line contains at least 2 params */
+    lastline = ptr;
+    if( !parse_next_param(&param, &param_size, &ptr, &size) ) { return NULL; }
+    if( !parse_next_param(&param, &param_size, &ptr, &size) ) { return NULL; }
+    return lastline;
 }
 
 static const char*
@@ -66,7 +125,25 @@ parse_params_find_negative(const char *text, int text_size) {
     return negative_found;
 }
 
-
+static void
+parse_sd_params_from_last_line(const char             *last_line,
+                               ParseParametersCallback callback,
+                               void                   *user_ptr,
+                               int                     user_int)
+{
+    const char *ptr, *key, *value, *param;
+    int size, key_size, value_size, param_size;
+    
+    ptr  = last_line;
+    size = strlen(last_line);
+    while( parse_next_param(&param, &param_size, &ptr, &size) ) {
+        parsed_param_key  ( &key  , &key_size  , param, param_size );
+        parsed_param_value( &value, &value_size, param, param_size );
+        if( key_size>0 && value_size>0 ) {
+            callback(key, key_size, value, value_size, user_ptr, user_int);
+        }
+    }
+}
 
 /* The parameter parsing logic used in this function is similar to that used
  * by the 'parse_generation_parameters' function in the AUTOMATIC1111 WebUI.
@@ -96,7 +173,7 @@ parse_sd_parameters(const char              *text,
     if( negative ) {
         negative_size = prompt_size - (negative - prompt);
         prompt_size   = (negative - prompt);
-        parsed_param_value( &negative, &negative_size );
+        parsed_param_value( &negative, &negative_size, negative, negative_size );
     }
     
     /* send extracted information */
@@ -106,9 +183,7 @@ parse_sd_parameters(const char              *text,
     if ( negative_size ) {
         callback("Negative prompt", 15, negative, negative_size, user_ptr, user_int);
     }
-    /*
     if( lastline ) {
-        parse_prompt_last_line( lastline, callback, user_ptr, user_int );
+        parse_sd_params_from_last_line( lastline, callback, user_ptr, user_int );
     }
-    */
 }
